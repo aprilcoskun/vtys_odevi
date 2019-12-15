edf805a205fa6d57184cd881799fc15b6c3403d2
Nisan <dev@alpcoskun.com>
2019-12-15 20:08:34 +0300

    user trigger added

---
+++ db/triggers.sql
@@ -0,0 +1,24 @@
+Create Trigger trg_deleted_users On Users FOR DELETE AS DECLARE @tc char(11),
+@name VARCHAR(50),
+@gender varchar(1),
+@password varchar(50),
+@birth_date date,
+@phone char(10),
+@user_height int,
+@user_weight int,
+@other_measurements varchar;
+
+SELECT @tc = del.tc FROM DELETED del;
+SELECT @name = del.name FROM DELETED del;
+SELECT @gender = del.gender FROM DELETED del;
+SELECT @password = del.password FROM DELETED del;
+SELECT @birth_date = del.birth_date FROM DELETED del;
+SELECT @phone = del.phone FROM DELETED del;
+SELECT @user_height = del.user_height FROM DELETED del;
+SELECT @user_weight = del.user_weight FROM DELETED del;
+SELECT @other_measurements = del.other_measurements FROM DELETED del;
+
+INSERT INTO DeletedUsers (tc, [password], gender, [name], birth_date, phone, user_height, user_weight) VALUES
+(@tc, @password, @gender, @name, @birth_date, @phone, @user_height, @user_weight)
+
+GO
