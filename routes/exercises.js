c54558d808b85c832d61a0838ec2dc564d7c5ba5
Nisan <dev@alpcoskun.com>
2020-01-02 17:42:45 +0300

    new icon

--- routes/exercises.js
+++ routes/exercises.js
@@ -4,12 +4,37 @@ const mssql = require('mssql');
 router.get('/', async function(req, res) {
   const result = await mssql.query`select * from exercises left join tools
 	on tools.id=exercises.tool_id for json auto`;
+  const result2 = await mssql.query`select * from tools`;
   
   res.render('exercises', {
     isAdmin: req.cookies['userType'] === 'trainer',
     isExercises: true,
-    exercises: result.recordset[0]
+    tools: result2.recordset,
+    exercises: result.recordset[0],
   });
+});
+
+router.post('/', async function(req, res) {
+  try {
+    const { name, description, set_size, tool_id } = req.body;
+    const result = await mssql.query`insert exercises(name, description, set_size, tool_id) values(${name}, ${description}, ${set_size}, ${tool_id})`;
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+router.put('/:id', async function(req, res) {
+  try {
+    const { id } = req.params;
+    const { name, description, set_size } = req.body;
+    const result = await mssql.query`update exercises set name=${name}, description=${description}, set_size=${set_size} where id=${id}`;
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
 
 router.get('/searchByName/:name', async function(req, res) {
